386fcf89528fa56a09e29e269b77a7abecefb577
Rafael Costa <rafael.costa@yaman.com.br>
2020-07-28 11:17:18 -0300

    UsuarioController e View Registro Criados

---
+++ GerenciadorCondominios/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorCondominios.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ GerenciadorCondominios/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Telefone { get; set; }
+
+        public string Foto { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "Senhas não conferem")]
+        public string SenhaConfirmada { get; set; }
+    }
+}
