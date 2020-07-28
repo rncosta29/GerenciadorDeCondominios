0c358a29b7e0b8b3b92e3e4fe1c62da1bce53c1b
Rafael Costa <rafael.costa@yaman.com.br>
2020-07-28 08:52:25 -0300

    Models Criado

---
+++ GerenciadorCondominios.BLL/Models/Evento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GerenciadorCondominios.BLL.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+
+        [Required(ErrorMessage = "O campo é {0} obrigatório")]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public DateTime Data { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ GerenciadorCondominios.BLL/Models/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GerenciadorCondominios.BLL.Models
+{
+    public class Veiculo
+    {
+        public int VeiculoId { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório!")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo marca é obrigatório!")]
+        [StringLength(25, ErrorMessage = "Use menos caracteres")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "O campo cor é obrigatório!")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        public string Cor { get; set; }
+
+        [Required(ErrorMessage = "O campo placa é obrigatório!")]
+        public string Placa { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+    }
+}
