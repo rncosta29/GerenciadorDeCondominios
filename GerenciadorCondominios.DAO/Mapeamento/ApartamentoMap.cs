48f397fcdf687ea2514dc17f248bfb7f31dbc0fc
Rafael Costa <rafael.costa@yaman.com.br>
2020-07-28 09:46:30 -0300

    Mapeamento e Contexto Criado

---
+++ GerenciadorCondominios.DAO/Mapeamento/ApartamentoMap.cs
@@ -0,0 +1,27 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominios.DAO.Mapeamento
+{
+    public class ApartamentoMap : IEntityTypeConfiguration<Apartamento>
+    {
+        public void Configure(EntityTypeBuilder<Apartamento> builder)
+        {
+            builder.HasKey(a => a.ApartamentoId);
+            builder.Property(a => a.Numero).IsRequired();
+            builder.Property(v => v.Andar).IsRequired();
+            builder.Property(v => v.Foto).IsRequired();
+            builder.Property(v => v.ProprietarioId).IsRequired();
+            builder.Property(v => v.MoradorId).IsRequired(false);
+
+            builder.HasOne(a => a.Proprietario).WithMany(a => a.ProprietariosApartamentos).HasForeignKey(a => a.ProprietarioId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(a => a.Morador).WithMany(a => a.MoradoresApartamentos).HasForeignKey(a => a.MoradorId).OnDelete(DeleteBehavior.NoAction);
+
+            builder.ToTable("Apartamentos");
+        }
+    }
+}
---
+++ GerenciadorCondominios.DAO/Mapeamento/HistoricoRecursoMap.cs
@@ -0,0 +1,26 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominios.DAO.Mapeamento
+{
+    public class HistoricoRecursoMap : IEntityTypeConfiguration<HistoricoRecurso>
+    {
+        public void Configure(EntityTypeBuilder<HistoricoRecurso> builder)
+        {
+            builder.HasKey(hr => hr.HistoricoRecursosId);
+            builder.Property(hr => hr.Valor).IsRequired();
+            builder.Property(hr => hr.Tipo).IsRequired();
+            builder.Property(hr => hr.Dia).IsRequired();
+            builder.Property(hr => hr.MesId).IsRequired();
+            builder.Property(hr => hr.Valor).IsRequired();
+
+            builder.HasOne(hr => hr.Mes).WithMany(hr => hr.HistoricoRecursos).HasForeignKey(hr => hr.MesId);
+
+            builder.ToTable("HistoricoRecursos");
+        }
+    }
+}
