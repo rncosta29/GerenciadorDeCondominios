cdbdc22bbf1bcaba82019e9306258aba6ec3e2ba
Rafael Costa <rafael.costa@yaman.com.br>
2020-07-28 10:19:34 -0300

    Interface e Repositorio Usuario Criado

---
+++ GerenciadorCondominios.DAO/Interfaces/IUsuarioGenerico.cs
@@ -0,0 +1,17 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.DAO.Interface
+{
+    public interface IUsuarioRepositorio : IRepositorioGenerico<Usuario>
+    {
+        int VerificarSeUsuarioExiste();
+        Task LogarUsuario(Usuario usuario, bool lembrar);
+        Task<IdentityResult> CriarUsuario(Usuario usuario, string senha);
+        Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao);
+    }
+}
---
+++ GerenciadorCondominios.DAO/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,73 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using GerenciadorCondominios.DAO.Interface;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.DAO.Repositorios
+{
+    public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
+    {
+        private readonly Contexto _contexto;
+        private readonly UserManager<Usuario> _gerenciadorUsuarios;
+        private readonly SignInManager<Usuario> _gerenciadorLogin;
+
+        public UsuarioRepositorio(Contexto contexto, UserManager<Usuario> gerenciadorUsuarios, SignInManager<Usuario> gerenciadorLogin) : base(contexto)
+        {
+            _contexto = contexto;
+            _gerenciadorUsuarios = gerenciadorUsuarios;
+            _gerenciadorLogin = gerenciadorLogin;
+        }
+
+        public async Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            try
+            {
+                return await _gerenciadorUsuarios.CreateAsync(usuario, senha);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            try
+            {
+                await _gerenciadorUsuarios.AddToRoleAsync(usuario, funcao);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task LogarUsuario(Usuario usuario, bool lembrar)
+        {
+            try
+            {
+                await _gerenciadorLogin.SignInAsync(usuario, lembrar);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int VerificarSeUsuarioExiste()
+        {
+            try
+            {
+                return _contexto.Usuarios.Count();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
